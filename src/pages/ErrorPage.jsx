bc632822869f32fabe47c91670376556d8eb7eea
JeffM20501 <kimmuna440@gmail.com>
2025-11-24 10:52:49 +0300

    .

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,7 +2,6 @@ import NavBar from "../components/NavBar"
 import { useRouteError } from "react-router-dom"
 function ErrorPage() {
     const error=useRouteError()
-    console.log(error)
     return (
         <div>
             <header><NavBar/></header>
